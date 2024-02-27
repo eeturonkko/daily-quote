82697785fd90cf7646d66382c4875ba0e6671f2b
Eetu Rönkkö <eetu.kristian.ronkko@gmail.com>
2024-02-27 13:30:06 +0200

    Refactor API call in +page.server.ts

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -8,15 +8,19 @@ export async function load({ fetch }) {
 	if (!apiKey) {
 		throw new Error('API key is not set');
 	}
-	const url = 'https://api.api-ninjas.com/v1/quotes?category=success';
-	const response = await fetch(url, {
-		headers: {
-			'x-api-key': apiKey
-		}
-	});
-	const quote: Quote[] = await response.json();
-	return {
-		quote: quote[0].quote,
-		author: quote[0].author
-	};
+	try {
+		const url = 'https://api.api-ninjas.com/v1/quotes?category=success';
+		const response = await fetch(url, {
+			headers: {
+				'x-api-key': apiKey
+			}
+		});
+		const quote: Quote[] = await response.json();
+		return {
+			quote: quote[0].quote,
+			author: quote[0].author
+		};
+	} catch (error) {
+		throw new Error('Failed to fetch quote');
+	}
 }
