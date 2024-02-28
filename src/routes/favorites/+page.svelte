1175033921ca3c5fe870728af890f693654fb948
Eetu Rönkkö <eetu.kristian.ronkko@gmail.com>
2024-02-28 19:13:12 +0200

    Refactor favorites list layout

--- src/routes/favorites/+page.svelte
+++ src/routes/favorites/+page.svelte
@@ -7,7 +7,7 @@
 	<div class="flex flex-col gap-2">
 		<h2 class="text-5xl font-semibold">Favorite quotes</h2>
 		<p class="text-lg font-medium">The best of the best.</p>
-		<ul>
+		<ul class="mt-6 grid grid-cols-2 gap-4">
 			{#each data.favorites as { quote, author }}
 				<li>
 					<blockquote>"{quote}"</blockquote>
