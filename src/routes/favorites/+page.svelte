74d004c28802a3e38849e7cf7c51caf3c8bfa320
Eetu Rönkkö <eetu.kristian.ronkko@gmail.com>
2024-02-28 19:14:51 +0200

    Update favorite quotes display

--- src/routes/favorites/+page.svelte
+++ src/routes/favorites/+page.svelte
@@ -1,16 +1,15 @@
 <script lang="ts">
 	export let data;
-	console.log(data);
 </script>
 
 <section class="mt-16 flex flex-col items-center gap-8 p-8 text-center">
 	<div class="flex flex-col gap-2">
 		<h2 class="text-5xl font-semibold">Favorite quotes</h2>
-		<p class="text-lg font-medium">The best of the best.</p>
+		<p class="text-lg font-medium">The best of the best. Chosen by you.</p>
 		<ul class="mt-6 grid grid-cols-2 gap-4">
 			{#each data.favorites as { quote, author }}
 				<li>
-					<blockquote>"{quote}"</blockquote>
+					<blockquote>{quote}</blockquote>
 					<cite>— {author}</cite>
 				</li>
 			{/each}
