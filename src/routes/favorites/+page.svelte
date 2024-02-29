063d98ccc885a7ea85f77f79798cba4d8180ae5c
Eetu Rönkkö <eetu.kristian.ronkko@gmail.com>
2024-02-29 11:52:46 +0200

    Add aria-label to favorite quotes list

--- src/routes/favorites/+page.svelte
+++ src/routes/favorites/+page.svelte
@@ -10,7 +10,7 @@
 	<section class="flex flex-col gap-2">
 		<h2 class="text-4xl font-semibold md:text-5xl">Favorite quotes</h2>
 		<p class="text-lg font-medium">The best of the best. Chosen by you.</p>
-		<ul class="mt-6 grid grid-cols-1 gap-4 md:grid-cols-2">
+		<ul aria-label="favorite quotes" class="mt-6 grid grid-cols-1 gap-4 md:grid-cols-2">
 			{#each data.favorites as { quote, author }}
 				<li>
 					<blockquote>{quote}</blockquote>
